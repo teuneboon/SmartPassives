acc0f065408dc1e3d3394f9d50a1dda0aedfe34e
teuneboon <teuneboon@gmail.com>
2015-03-17 21:58:12 +0100

    first start

---
+++ application/src/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public void run() {
+        System.out.println("Test");
+    }
+
+    public static void main(String[] args) {
+        new Application().run();
+    }
+}
